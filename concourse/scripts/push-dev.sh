8c939ea0fa7f741ab6994b306fd562f5935c9664
Mithilan Sivanesan <mithilan_sivanesan@manulife.com>
2020-12-14 20:54:30 -0500

    typos

--- concourse/scripts/push-dev.sh
+++ concourse/scripts/push-dev.sh
@@ -4,7 +4,6 @@ set -x
 
 # Get Commit-SHA
 cd source
-ls
 pwd
 commitsha=`git rev-parse HEAD`
 #download CodeQL- runner
@@ -15,6 +14,7 @@ chmod +x codeql-runner-linux
 cd source 
 ls
 ../codeql-runner-linux init --repository manulife-ets/mith_calculator --github-url https://github.com  --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228
+ls
 ../codeql-runner-linux analyze --repository manulife-ets/mith_calculator --github-url https://github.com --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228 --ref refs/heads/ghas --commit "$commitsha"
 
 # cd source
