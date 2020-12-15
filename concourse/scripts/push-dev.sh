7b07f27266ee77e3f329fb773d863f59a0cf03d5
Mithilan Sivanesan <mithilan_sivanesan@manulife.com>
2020-12-15 00:28:47 -0500

    analyze step from with in repo

--- concourse/scripts/push-dev.sh
+++ concourse/scripts/push-dev.sh
@@ -14,7 +14,7 @@ chmod +x codeql-runner-linux
 #Init and Analyze
 cd "$sourcedir"
 ls
-./codeql-runner-linux init --repository manulife-ets/mith_calculator --temp-dir "$workingdir/codeql-runner" --checkout-path "$sourcedir" --github-url https://github.com  --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228
+../codeql-runner-linux init --repository manulife-ets/mith_calculator --temp-dir "$workingdir/codeql-runner" --checkout-path "$sourcedir" --github-url https://github.com  --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228
 ls
 ../codeql-runner-linux analyze --repository manulife-ets/mith_calculator --temp-dir "$workingdir/codeql-runner" --checkout-path "$sourcedir" --github-url https://github.com --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228 --ref refs/heads/ghas --commit "$commitsha"
 
