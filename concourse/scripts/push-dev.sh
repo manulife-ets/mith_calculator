3fdc5b8627c5db87995eca6bd6b738779e4e8565
Mithilan Sivanesan <mithilan_sivanesan@manulife.com>
2020-12-14 20:32:34 -0500

    add ls

--- concourse/scripts/push-dev.sh
+++ concourse/scripts/push-dev.sh
@@ -4,6 +4,7 @@ set -x
 
 # Get Commit-SHA
 cd source
+ls
 pwd
 commitsha=`git rev-parse HEAD`
 #download CodeQL- runner
@@ -12,6 +13,7 @@ wget https://github.com/github/codeql-action/releases/download/codeql-bundle-202
 chmod +x codeql-runner-linux
 #Init and Analyze
 cd source 
+ls
 ../codeql-runner-linux init --repository manulife-ets/mith_calculator --github-url https://github.com  --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228
 ../codeql-runner-linux analyze --repository manulife-ets/mith_calculator --github-url https://github.com --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228 --ref refs/heads/ghas --commit "$commitsha"
 
