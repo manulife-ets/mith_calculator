17cc37b93fdc2045afb61ea4ed88a97eee3989b5
Mithilan Sivanesan <mithilan_sivanesan@manulife.com>
2020-12-15 00:01:32 -0500

    analyze step from with in repo

--- concourse/scripts/push-dev.sh
+++ concourse/scripts/push-dev.sh
@@ -16,7 +16,7 @@ ls
 ./codeql-runner-linux init --repository manulife-ets/mith_calculator  --github-url https://github.com  --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228
 cd "$sourcedir"
 ls
-./codeql-runner-linux analyze --repository manulife-ets/mith_calculator --github-url https://github.com --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228 --ref refs/heads/ghas --commit "$commitsha"
+../codeql-runner-linux analyze --repository manulife-ets/mith_calculator --github-url https://github.com --github-auth b6bbe465b565994fbd8c4cef4af6f4a737f83228 --ref refs/heads/ghas --commit "$commitsha"
 
 # cd source
 
